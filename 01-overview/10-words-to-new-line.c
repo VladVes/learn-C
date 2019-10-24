c87c90b5807012fb6a88c9fa75dc0ab30d094ec9
VladVes <veselov.workflow@gmail.com>
2019-10-24 16:54:51 +0700

    add words to new line

---
+++ 01-overview/10-words-to-new-line.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int ch;
+    while ((ch = getchar()) != EOF) {
+        if (ch == ' ' || ch == '\t') putchar('\n');
+        else putchar(ch);
+    }
+    return 0;
+}
